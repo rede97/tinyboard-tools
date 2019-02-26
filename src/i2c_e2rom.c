c555de387a2e3014227f77fb91721d6ac862afad
hqm <1789339038@qq.com>
2019-02-26 18:51:43 +0800

    增加 erase

--- src/i2c_e2rom.c
+++ src/i2c_e2rom.c
@@ -5,8 +5,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <windows.h>
+#include <time.h>
+
+#define AT24C256B_ADDR  0x50
+#define EEPROM_BATCH_CAPCITY 0x800
+#define EEPROM_DELAY  3
 
 const char * help = "Usage: i2cdump I2CBUS ADDRESS\n I2CBUS is an integer\n ADDRESS is an hex integer (0x03 - 0x77)\n";
+
 
 void i2c_read(FT_HANDLE handle, uint16_t reg, uint8_t *dat,uint8_t ndat);
 void i2c_write(FT_HANDLE handle, uint16_t addr,uint8_t dat);
@@ -16,7 +22,7 @@ void i2c_read(FT_HANDLE handle, uint16_t reg, uint8_t *dat,uint8_t ndat) {
   uint8_t serial[1024];
   uint32_t restart_point, nbytes;
   //start + addr + w +
-  uint32_t len = i2c_begin(serial, 0x50); //开始写 
+  uint32_t len = i2c_begin(serial, AT24C256B_ADDR); //开始写 
   //高8位地址+
   len += i2c_send(serial + len,(reg>>8));
   //低8位地址+
@@ -26,7 +32,7 @@ void i2c_read(FT_HANDLE handle, uint16_t reg, uint8_t *dat,uint8_t ndat) {
 
   //start + addr + r +  接收 + stop
   restart_point = len;
-  len += i2c_request(serial+len,0x50,ndat,1);
+  len += i2c_request(serial+len,AT24C256B_ADDR,ndat,1);
   
   FT_Write(handle, serial, len, (LPDWORD)&nbytes);
   FT_Read(handle, serial, nbytes, (LPDWORD)&nbytes);
@@ -40,7 +46,7 @@ void i2c_write(FT_HANDLE handle, uint16_t addr,uint8_t dat)
   //发送I2C开始信号 start+
   uint32_t len = i2c_start(serial);
   //写地址  addr + w +
-  len += i2c_send(serial + len, 0x50<<1);  
+  len += i2c_send(serial + len, AT24C256B_ADDR<<1);  
   //发送高8位地址 high8bit+
   len += i2c_send(serial + len, addr>>8);
   //发送低8位地址 low8bit+
@@ -52,7 +58,7 @@ void i2c_write(FT_HANDLE handle, uint16_t addr,uint8_t dat)
 
    FT_Write(handle, serial, len, (LPDWORD)&nbytes);
    FT_Read(handle, serial, nbytes, (LPDWORD)&nbytes);
-   Sleep(10);
+   Sleep(EEPROM_DELAY);
 }
 
 void e2prom_write_8bit(FT_HANDLE handle, uint16_t addr, uint8_t dat)
@@ -74,6 +80,12 @@ void e2prom_dump(FT_HANDLE handle, uint8_t *dat, uint16_t ndat)
   for(int i=0x0000;i<ndat;i+=step)
    i2c_read(handle,i,dat+i,step);
  
+}
+
+void e2prom_erase(FT_HANDLE handle)
+{
+  for(int i=0;i<EEPROM_BATCH_CAPCITY;i++)
+   e2prom_write_8bit(handle,i,0xff);
 }
 
 void e2prom_dump_output(uint8_t *dat,uint16_t length)
@@ -149,10 +161,20 @@ int main(int argc, char *argv[])
 
   FT_Purge(handle, FT_PURGE_RX);
 
-  e2prom_write_16bit(handle,0x0000,0x1234);
+  clock_t start,finish;
+  double duration;
 
+  start = clock();
+  // e2prom_write_16bit(handle,0x0000,0x1234);
+  //e2prom_erase(handle);
   e2prom_dump(handle,dat,ndat);
   e2prom_dump_output(dat,ndat);
 
+  finish = clock();
+
+  duration = (double)(finish - start) / CLOCKS_PER_SEC;
+  printf("\ntime duration:%0.3fs\n",duration);
+
+ 
   return 0;
 }
